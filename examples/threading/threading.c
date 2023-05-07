1a08473591c31a4f217d7c05f26b67ea8a8b1848
leafpmpmp <leafpmpmp@gmail.com>
2023-05-07 09:55:31 +0800

    Assignement 4 part 1 submit 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,26 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    thread_func_args->thread_complete_success=false;
+    *(thread_func_args->thread)=pthread_self();
+
+    usleep((thread_func_args->wait_to_obtain_ms)*1000);
+    int retLock=pthread_mutex_lock(thread_func_args->mutex);
+    if(retLock==0){
+        usleep((thread_func_args->wait_to_release_ms)*1000);
+        int retUnLock=pthread_mutex_unlock(thread_func_args->mutex);
+        if(retUnLock==0){
+            thread_func_args->thread_complete_success=true;
+            DEBUG_LOG("mutex locked and released successfully\n");
+        }
+        else
+            ERROR_LOG("pthread_mutex_unlock failed with return=%d\n", retUnLock);
+    }
+    else
+        ERROR_LOG("pthread_mutex_lock failed with return=%d\n", retLock);
+
     return thread_param;
 }
 
@@ -28,6 +48,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    struct thread_data* thread_param = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    if(thread_param!=NULL){
+        thread_param->thread = thread;
+        thread_param->mutex = mutex;
+        thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+        thread_param->wait_to_release_ms = wait_to_release_ms;
+        pthread_t t;
+        int ret = pthread_create( &t, NULL, threadfunc, thread_param);
+        if(ret==0){
+            DEBUG_LOG("Thread created successfully\n");
+            return true;
+        }
+        else
+            ERROR_LOG("pthread_create failed with return=%d\n", ret);
+
+    }
+    DEBUG_LOG("malloc failed\n");
+
     return false;
 }
 
